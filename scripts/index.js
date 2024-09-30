bdc2c1ea54b86a183dc23d27174048369c178909
Husam Bahra <husamsrock@gmail.com>
2024-09-30 17:44:52 -0400

    New updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const initialcards = [
+const initialCards = [
   {
     name: Kapadokya,
     link: "https://images.unsplash.com/photo-1526048598645-62b31f82b8f5?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
